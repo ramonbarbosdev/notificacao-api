30d887bc0db3e0a513a10a8b2485b4982e657e9f
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2026-05-15 17:49:42 -0300

    feat: cors

--- src/main/java/com/notificacao_api/config/SecurityConfiguracao.java
+++ src/main/java/com/notificacao_api/config/SecurityConfiguracao.java
@@ -75,7 +75,8 @@ public class SecurityConfiguracao {
                 "http://localhost:4200",
                 "http://127.0.0.1:4200",
                 "http://localhost:5173",
-                "http://127.0.0.1:5173"));
+                "http://127.0.0.1:5173",
+            "https://notificacao.ramoncode.com.br"));
         configuracao.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuracao.setAllowedHeaders(Arrays.asList("*"));
         configuracao.setAllowCredentials(true);
--- src/main/java/com/notificacao_api/config/WebSocketConfiguracao.java
+++ src/main/java/com/notificacao_api/config/WebSocketConfiguracao.java
@@ -14,7 +14,8 @@ public class WebSocketConfiguracao implements WebSocketMessageBrokerConfigurer {
             "http://localhost:4200",
             "http://127.0.0.1:4200",
             "http://localhost:5173",
-            "http://127.0.0.1:5173"
+            "http://127.0.0.1:5173",
+            "https://notificacao.ramoncode.com.br"
     };
 
     @Override
